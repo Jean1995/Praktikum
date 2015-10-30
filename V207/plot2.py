17f0419f925b6097b0a1619409d4067d15f955b8
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2015-10-31 00:25:39 +0100

    Start Abstandmessung

--- V207/plot2.py
+++ V207/plot2.py
@@ -30,6 +30,26 @@ np.savetxt('ausgleichswerte_d.txt', np.column_stack([par1, fehler]), header= "d 
 
 ## Unsere Ausgleichsrechnung:
 
+y = Uabs
+x = 1/d**2
+
+b = ( np.sum(x**2) * np.sum(y) - np.sum(x) * np.sum(x*y) ) / ( 12 * np.sum(x**2) - np.sum(x)**2 )
+m = ( 12 * np.sum(x*y) - np.sum(x) * np.sum(y) ) / ( 12 * np.sum(x**2) - np.sum(x)**2 )
+
+delta_y = y-b-m*x
+sy_quadrat = ( np.sum(delta_y**2) ) / (12 - 2)
+
+sigma_b_quadrat = sy_quadrat * ( np.sum(x**2) ) / ( 12 * np.sum(x**2) - np.sum(x)**2 )
+sigma_m_quadrat = sy_quadrat * ( 12 ) / ( 12 * np.sum(x**2) - np.sum(x)**2 )
+
+mb = np.array([m, b])
+mb_sigma = np.array([np.sqrt(sigma_m_quadrat), np.sqrt(sigma_b_quadrat)])
+
+
+np.savetxt('Uabs_Ausgleichswerte', np.column_stack([mb, mb_sigma]), header="m/b m_err/b_err" )
+
+##
+
 plt.xlabel(r'$d \:/\: \si{\metre}$')
 plt.ylabel(r'$U_d \:/\: \si{\volt}$')
 plt.legend(loc='best')
