f4036b4b2f138d2cf4ae8659426f19ac608cd284
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2015-11-05 00:00:49 +0100

    Vorlage erstellt (mit .gitignore, table.py, header) V206: Gitignore hinzugefügt V207: Gitignore hinzugefügt Roberto: Entfernt

---
+++ Vorlage/plot.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from table import (
+    make_table,
+    make_SI,
+    write,
+)
+from uncertainties import ufloat
+
+# Beispielplot
+x = np.linspace(0, 10, 1000)
+y = x ** np.sin(x)
+plt.plot(x, y, label='Kurve')
+plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.legend(loc='best')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot.pdf')
+
+
+# Beispieltabelle
+a = np.linspace(1, 10, 10)
+b = np.linspace(11, 20, 10)
+write('build/tabelle.tex', make_table([a, b], [4, 2]))   # [4,2] = Nachkommastellen
+
+
+# Beispielwerte
+
+
+c = ufloat(24601, 42)
+write('build/wert_a.tex', make_SI(c*1e3, r'\joule\per\kelvin\per\gram' ))
