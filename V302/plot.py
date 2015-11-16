08ffe39c00b0d89da4146f1e544d1c648ecc9949
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2015-11-16 18:26:38 +0100

    V302: Auswertung korrigiert und Diskussion

--- V302/plot.py
+++ V302/plot.py
@@ -64,7 +64,7 @@ write('build/kapa/kapa.R8_3.tex', make_SI(Rx[2], r'\ohm', figures=1))
 write('build/kapa/kapa.R8m.tex', make_SI(Rx_mean, r'\ohm', figures=1))
 
 #Kondensator Wert 3
-C_2, C_2er, R_2, R_2er, R_3, R_4, baz, bar = np.genfromtxt('kapa.wert3.txt', unpack=True)
+C_2, C_2er, R_2, R_2er, R_3, R_4, R_34, R_34er = np.genfromtxt('kapa.wert3.txt', unpack=True)
 C_2 = C_2*10**(-9)
 C_2er = C_2er*10**(-9)
 C2  = unp.uarray(C_2, C_2er)
@@ -72,11 +72,15 @@ R2  = unp.uarray(R_2, R_2er)
 R34 = unp.uarray(R_34, R_34er)
 #Hab den Mist aufgegeben: Diese sch**** Tabelle wird jetzt von Hand erstellt. Punkt!
 #write('build/kapa2tabelle.tex', make_table([C2*10**(9), R2, baz, bar], [1, 1, 1, 1, 1, 1]))
+ctmp0 = R34[0]
+#Cx = unp.uarrray([])
 Cx = C2*1/R34
 Cx_mean=np.mean(Cx)
 
 Rx = R2*R34
 Rx_mean = np.mean(Rx)
+
+write('build/kapa/kapa.C3_1tmp.tex', make_SI(ctmp0, r'\nano\farad', figures=1))
 
 write('build/kapa/kapa.C3_1.tex', make_SI(Cx[0]*10**9, r'\nano\farad', figures=1))
 write('build/kapa/kapa.C3_2.tex', make_SI(Cx[1]*10**9, r'\nano\farad', figures=1))
@@ -175,7 +179,7 @@ plt.xscale('log')
 plt.ylim(0,0.4)
 
 v= v*v0
-v0 = 1498.07 #errechneter Wert mit C_3 und R
+v0 = 1149.6 #errechneter Wert mit C_3 und R
 v = v/v0
 # fitten
 from scipy.optimize import curve_fit
@@ -191,7 +195,7 @@ plt.legend(loc='best')
 #f)
 
 U_br = U[11] #Brückenspannung bei Minimum
-Br_Bs = np.sqrt(f(2)) #Br/Bs Faktor für omega = 2, da omega = w/w0 = v/v0 = 2*v0/v0 = 2
+Br_Bs = f(2) #Br/Bs Faktor für omega = 2, da omega = w/w0 = v/v0 = 2*v0/v0 = 2
 U2 = U_br / Br_Bs
 k = U2/Us
 write('build/klirrfaktor.tex', str(k))
@@ -214,7 +218,7 @@ write('build/klirrfaktor.tex', str(k))
 #plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
 #plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
 #plt.legend(loc='best')
-#
+#-
 ## in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot.pdf')
